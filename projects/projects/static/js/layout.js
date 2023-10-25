349d4c31a421f3f687baaafd25795f3ba798b78d
yahyamohmuedpro99 <133171914+yahyamohmuedpro99@users.noreply.github.com>
2023-10-25 23:41:54 +0300

    Delete projects/projects directory
    resturcure

--- projects/projects/static/js/layout.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $(".menu ul").on("click",function(){
-        $("li").slideToggle()
-    })
-    
-})
--- projects/projects/templatetags/commentTemp.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from Users.models import *
-register = template.Library()
-
-@register.filter
-def filter_comment_report(comment_id,user_current):
-    return not (Reportno.objects.filter(comment_id=comment_id,user_id=user_current).exists() )
-    
-
