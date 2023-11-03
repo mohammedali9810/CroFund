be9d9643afb65922fbc94817c2801cc3907e0abd
Mohammed Ali <mohammedali9810@gmail.com>
2023-11-03 13:24:01 +0200

    Some mods to stash.

--- projects/admin.py
+++ projects/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Projects
+from .models import Projects, Categories
 # Register your models here.
 admin.site.register(Projects)
+admin.site.register(Categories)
